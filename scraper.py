ec39294a0b525413f9af806facb9dcb90b813d6a
Moshe Bas <moshe.bas@gmail.com>
2021-07-23 10:05:34 +0300

    Added a data generator.

--- scraper.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup as bs
-import requests
-
-r = requests.get('https://tabs.ultimate-guitar.com/tab/pink-floyd/shine-on-you-crazy-diamond-part-i-v-tabs-513655')
-
-print(bs(r.text))
